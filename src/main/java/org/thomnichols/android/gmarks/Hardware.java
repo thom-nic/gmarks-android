cac1e1e71801d5554d940b6b3fac57f1a1bc1482
Thom Nichols <tmnichols@gmail.com>
2011-01-28 19:39:08 -0500

    updated hardware list with IDs from GLBenchmark

--- src/main/java/org/thomnichols/android/gmarks/Hardware.java
+++ src/main/java/org/thomnichols/android/gmarks/Hardware.java
@@ -12,6 +12,7 @@ public final class Hardware {
 		Log.d("GMARKS HARDWARE", "This device's Build.MODEL is " + Build.MODEL);
 		/* Many of these Build.MODEL codes have come from
 		 * http://en.wikipedia.org/wiki/Comparison_of_Android_devices
+		 * and http://www.glbenchmark.com/result.jsp
 		 */
 		final Set<String> s = HAS_SEARCH_KEY;
 //		s.add("google_sdk"); // Debug only
@@ -31,30 +32,31 @@ public final class Hardware {
 		s.add("HTC+Tattoo"); // AKA 'Click' 
 		s.add("PC36100"); // HTC Evo 4G 
 		s.add("Nexus+One");
+		s.add("Nexus One"); // according to GLBenchmark
 		s.add("HTC Ace"); // Desire HD / MyTouch 4G
 		s.add("HTC Wildfire");
 		s.add("LG-VS740"); // LG Ally
 		// TODO LG Axis
 		s.add("MB501"); // Motorola Cliq XT 
 		s.add("ME502"); // Motorola Charm
+		s.add("WX445"); // Motorola Citrus
 		s.add("Droid"); // AKA Milestone
 		s.add("DROIDX"); 
 		s.add("DROID2"); 
+		s.add("Nexus S");
 		s.add("SCH-r880"); // Samsung Acclaim (US Cellular) 
 		s.add("SGH-I897"); // Samsung Captivate 
+		s.add("GT-I897"); // Samsung Captivate 
+		s.add("SGH-T959D"); // Samsung Vibrant
+		s.add("SGH-T959"); // Samsung Vibrant
+		s.add("SCH-I500"); // Samsung Fascinate/ Showcase/ Mesmerize 
+		s.add("SPH-D700"); // Samsung/ Sprint Epic 4G
 		s.add("M910"); // Samsung Intercept
+		s.add("SPH-M910"); // Samsung Intercept
 		// the following are un-confirmed:
 		s.add("MB525"); // Motorola Defy (guess) 
 		s.add("DROIDPRO"); // TODO (guess) 
-		s.add("SGH-T959"); // Samsung Vibrant (guess) 
-		s.add("SGH-t959"); // Samsung Vibrant (guess) 
-		s.add("SCH-I500"); // Samsung Fascinate/ Showcase/ Mesmerize (guess) 
-		s.add("SCH-i500"); // Samsung Fascinate/ Showcase/ Mesmerize (guess) 
-		s.add("SPH-D700"); // Samsung/ Sprint Epic 4G (guess) 
-		s.add("SPH-d700"); // Samsung/ Sprint Epic 4G (guess)
-		s.add("D700"); // Samsung/ Sprint Epic 4G (guess) 
 		s.add("SPH-M920"); // Samsung Transform (guess) 
-		s.add("SPH-m920"); // Samsung Transform (guess) 
 		s.add("M920"); // Samsung Transform (guess) 
 		s.add("GT-i9020"); // Nexus S (guess) 
 //		s.add(""); //  
